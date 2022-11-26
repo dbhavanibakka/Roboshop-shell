33f8a991a62f770f3007f17284b3a2611bc4c501
ruthwik <dbhavanibakka@gmail.com>
2022-11-26 11:18:24 +0530

    frontend script

---
+++ mongodb.sh
@@ -0,0 +1,10 @@
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo# yum install -y mongodb-org
+                                                                                                                          # systemctl enable mongod
+                                                                                                                          # systemctl start mongod
+yum install -y mongodb-org
+systemctl enable mongod
+systemctl start mongod
+
+
+
+
