4019fbf707f3e327282d20c8a7197a8a2b984dc6
ruthwik <dbhavanibakka@gmail.com>
2022-11-26 20:26:46 +0530

    var script

--- frontend.sh
+++ frontend.sh
@@ -10,15 +10,12 @@ systemctl start nginx
 echo -e "\e[33mdownload the nginx\e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
-
 cd /usr/share/nginx/html
 rm -rf *
-
 echo -e "\e[34frontend  unzip\e[0m"
 unzip -o /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-
 
 echo -e "\e[35restart the nginx\e[0m"
 systemctl restart nginx
