308b08f6eb42619e5400bc7af9483ab6453c1769
ruthwik <dbhavanibakka@gmail.com>
2022-11-26 20:31:27 +0530

    var script

--- frontend.sh
+++ frontend.sh
@@ -12,10 +12,7 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/fron
 
 cd /usr/share/nginx/html
 rm -rf *
-
-echo -e "\e[34frontendunzip\e[0m"
 unzip -o /tmp/frontend.zip
-
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
